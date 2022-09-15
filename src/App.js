9a8394a3354a0234c840e14626a7e9ebf1e9b166
sicwere <sicwere@users.noreply.github.com>
2022-09-14 22:04:36 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <p>Hello CSCI E-39!</p>
+        <p>
+          My name is Harry Ferguson
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
